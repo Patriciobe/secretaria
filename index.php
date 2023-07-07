338f83fb7454fd77bf11af6edae8206ca0fd9306
patriciobe <patriciovaldez944@gmail.com>
2023-07-06 19:03:43 -0500

    eliminandobasura

--- index.php
+++ index.php
@@ -38,24 +38,8 @@
 
 
     <div id="datos"></div>
-    <div id="form">
-  <form action="subir.php" method="POST" enctype="multipart/form-data">
-    <div>
-      <div>   
-        <label for="file">Subir imagen</label>  
-      </div>
-      <input type="file" id="file" name="file">  
-    </div> 
-    <div>
-      <div> 
-        <label for="desc">Descripción</label> 
-      </div> 
-      <input type="text" id="desc" name="desc">  
-    </div>
-    <div>    
-      <input type="submit" value="Subir">  
-    </div>                      
-  </form>
+<div id="form">
+    <?php include_once 'views/form.php'?>
 </div>
 <!-- Inicializa DataTables con responsividad de columnas -->
 
---
+++ views/form.php
@@ -0,0 +1,17 @@
+<form action="subir.php" method="POST" enctype="multipart/form-data">
+    <div>
+      <div>   
+        <label for="file">Subir imagen</label>  
+      </div>
+      <input type="file" id="file" name="file">  
+    </div> 
+    <div>
+      <div> 
+        <label for="desc">Descripción</label> 
+      </div> 
+      <input type="text" id="desc" name="desc">  
+    </div>
+    <div>    
+      <input type="submit" value="Subir">  
+    </div>                      
+</form>
