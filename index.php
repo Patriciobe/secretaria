173a3203540965abdd7977e37fe955a47db386fa
patriciobe <patriciovaldez944@gmail.com>
2023-07-06 22:13:52 -0500

    prefix

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
 
 <div class="datatables">
      <?php include_once 'views/tabla.php'?>
+     <!-- Inicializa DataTables con responsividad de columnas -->
 </div>
 
 
@@ -44,30 +45,10 @@
 
 
 <div id="form">
-  <form action="upload.php" class="dropzone" id="myDropzone">
-
-    <div>
-      <div>
-      <label for="file-name">Nombre del archivo:</label>
-      </div>
-      <input type="text" id="file-name" name="file-name" placeholder="Ingrese un nombre">
-    </div>
-
-    <div>
-      <div>
-      <label for="desc">Descripción:</label>
-      </div>
-      <input type="text" id="desc" name="desc" placeholder="Ingrese una descripción">
-    </div>
-    <div class="dz-message">
-      Arrastra y suelta los archivos aquí o haz clic para seleccionar archivos.
-    </div>
-  
-  </form>
-
-  
+     <?php include_once 'views/form.php'?>
+      <!-- dropzonejs -->
 </div>
-<!-- Inicializa DataTables con responsividad de columnas -->
+
 
 
 
---
+++ views/form.php
@@ -0,0 +1,20 @@
+<form action="upload.php" class="dropzone" id="myDropzone">
+
+<div>
+  <div>
+  <label for="file-name">Nombre del archivo:</label>
+  </div>
+  <input type="text" id="file-name" name="file-name" placeholder="Ingrese un nombre">
+</div>
+
+<div>
+  <div>
+  <label for="desc">Descripción:</label>
+  </div>
+  <input type="text" id="desc" name="desc" placeholder="Ingrese una descripción">
+</div>
+<div class="dz-message">
+  Arrastra y suelta los archivos aquí o haz clic para seleccionar archivos.
+</div>
+
+</form>
